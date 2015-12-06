c0221dbc6750ee24ccc1789788553b4393421087
Guoxi Li <goksyli1990@gmail.com>
2015-12-06 17:31:12 +0800

    add add_node.sh file

---
+++ add_node.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+set -e
+
+
+
+function prepare_add() {
+
+    source "${KUBE_ROOT}/cluster/ubuntu/util.sh"
+    source "${KUBE_ROOT}/cluster/ubuntu/config-default.sh"
+
+    setClusterInfo
+
+}
+
+
+function add-node() {
+    # add node to current cluster
+    
+    
+
+}
